408e71c0f6806663b68cb4e90ae16910c96b01af
Dondre Mclenan <d.mclenan@gmail.com>
2022-02-17 23:27:31 -0500

    nodes

--- models/index.js
+++ models/index.js
@@ -3,33 +3,33 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
 
-// User.hasMany(Post, {
-//   foreignKey: 'userId',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Post, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
 
-// User.hasMany(Comment, {
-//   foreignKey: 'userId',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
 
-// Post.belongsTo(User, {
-//   foreignKey: 'userId',
-//   onDelete: 'CASCADE'
-// });
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
 
-// Post.hasMany(Comment, {
-//   foreignKey: 'postId',
-//   onDelete: 'CASCADE'
-// });
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
 
-// Comment.belongsTo(User, {
-//   foreignKey: 'userId',
-// });
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
 
-// Comment.belongsTo(Post, {
-//   foreignKey: 'postId'
-// });
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
 
 module.exports = {
   User,
